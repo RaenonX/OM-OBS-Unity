7a80395991c486dabd5013b946504a3a4f270355
ryoliao <ryo@mochi.com.tw>
2020-06-05 19:42:46 +0800

    ADD - Animated Marquee Effect

--- Assets/Scripts/OM.OBS/AnimatedText.cs
+++ Assets/Scripts/OM.OBS/AnimatedText.cs
@@ -1,10 +1,25 @@
 ﻿
 using UnityEngine;
 using TMPro;
+using UnityEngine.Playables;
 
 namespace OM.OBS
 {
     public class AnimatedText : MonoBehaviour
     {
+        public PlayableDirector Director;
+        public TextMeshProUGUI AppearText;
+        public TextMeshProUGUI DissolveText;
+
+        public string text
+        {
+            set
+            {
+                DissolveText.text = AppearText.text;
+                AppearText.text = value;
+                Director.time = 0;
+                Director.Play();
+            }
+        }
     }
 }
